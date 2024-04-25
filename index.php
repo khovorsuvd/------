99bf23aff4f4702c102c0391c92e98f78252427a
khovorsuvd <rrrrr_rrrrrrrrr_rrrrrrr@mail.ru>
2024-04-25 08:51:01 +0300

    t

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         
 
     <link rel="stylesheet" href="./css/style.css">
-<script src="./js/jq.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="./js/script.js"></script>
 
 </head>
